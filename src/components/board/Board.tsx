a592bf8831e7e56e6d1a0cca7c752c1bc67809ff
unknown <zoran.zilic.zack@gmail.com>
2022-07-03 01:52:18 +0200

    Add Phases style to the Moon.

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,22 +1,18 @@
 import { useContext } from "react";
 import { SolarSystem, Sidebar, MoonPhases, Language, DownloadPDF, CalendarVisualisation } from "../";
 import { BoardContext, TestContext } from "../../context";
-import TestSideralVsSynodic from "../solarSystem/TestSideralVsSynodic";
 
 const Board = () => {
     const { isGeorgianCallendar, setIsGeorgianCallendar } = useContext(BoardContext);
     const { isTestMode, setIsTestMode } = useContext(TestContext);
     const textModeText: string = isTestMode ? "hide test mode" : "show test mode";
-    const georgianOrHebrewCallendarText: string = isGeorgianCallendar ? "Show Hebrew Callendar" : "Show Georgian Callendar";
 
     return (
         <div className="board_wrapper">       
             <button style={{position: "absolute", top: 0, left: 0, zIndex: 2}} onClick={() => setIsTestMode(!isTestMode)}> {textModeText} </button>      
-            <button style={{position: "absolute", top: "20px", left: 0, zIndex: 2}} onClick={() => setIsGeorgianCallendar(!isGeorgianCallendar)}> {georgianOrHebrewCallendarText} </button>      
             <CalendarVisualisation />
             <SolarSystem />
             <Sidebar />
-            <TestSideralVsSynodic />
             <Language />
             <MoonPhases />
             <DownloadPDF />
--- src/components/moon/MoonPhases.tsx
+++ src/components/moon/MoonPhases.tsx
@@ -4,10 +4,7 @@ import { BoardContext } from "../../context";
 import SelectedDate from "./SelectedDate";
 
 const MoonPhases = () => {
-    const { 
-        simulationNumberOfDays, 
-        synodicPeriodNumberOfDays
-    } = useContext(BoardContext);
+    const { simulationNumberOfDays, synodicPeriodNumberOfDays } = useContext(BoardContext);
     
     const numberOfSynodicPeriod: number = Math.abs(simulationNumberOfDays) / synodicPeriodNumberOfDays;
     const progressOfTheMoonMovementInSynodicPeriodInPercentage: number = numberOfSynodicPeriod - Math.floor(numberOfSynodicPeriod);
@@ -15,56 +12,49 @@ const MoonPhases = () => {
 
     const value: number = progressOfTheMoonMovementInSynodicPeriodInDays;
 
-    const shadowAcordingTosimulationNumberOfDays = () => {
-        if (value < 1    ) return {transform: "translateX(0vh)"}; // 1
-        if (value >= 1 && value <  2 ) return {transform: "translateX(-2vh)"}; // 1
-        if (value >= 2 && value <  3 ) return {transform: "translateX(-4vh)"}; // 2
-        if (value >= 3 && value <  4 ) return {transform: "translateX(-6vh)"}; // 3
-        if (value >= 4 && value <  5 ) return {transform: "translateX(-8vh)"}; // 4
-       
-        if (value >= 5 && value <  6 ) return {transform: "translateX(-10vh)"}; // 5
-        if (value >= 6 && value <  7 ) return {transform: "translateX(-12vh)"}; // 6
-        if (value >= 7 && value <  8 ) return {transform: "translateX(-13vh)"}; // 7
-        if (value >= 8 && value <  9 ) return {transform: "translateX(-14vh)"}; // 8
-        if (value >= 9 && value <  10 ) return {transform: "translateX(-16vh)"}; // 9
-        
-        if (value >= 10 && value <  11 ) return {transform: "translateX(-18vh)"}; // 10
-        if (value >= 11 && value <  12 ) return {transform: "translateX(-20vh)"}; // 11
-        if (value >= 12 && value <  13 ) return {transform: "translateX(-22vh)"}; // 12
-        if (value >= 13 && value <  14 ) return {transform: "translateX(-23vh)"}; // 13
-        if (value >= 14 && value <  15 ) return {transform: "translateX(24vh)"}; // 14
-        
-        if (value >= 15 && value <  16 ) return {transform: "translateX(23vh)"}; // 15
-        if (value >= 16 && value <  17 ) return {transform: "translateX(22vh)"}; // 16 
-        if (value >= 17 && value <  18 ) return {transform: "translateX(20vh)"}; // 17
-        if (value >= 18 && value <  19 ) return {transform: "translateX(18vh)"}; // 18
-        if (value >= 19 && value <  20 ) return {transform: "translateX(16vh)"}; // 19
-        
-        if (value >= 20 && value < 21   ) return {transform: "translateX(14vh)"}; // 20
-        if (value >= 21 && value < 22   ) return {transform: "translateX(12vh)"}; // 21 
-        if (value >= 22 && value < 23   ) return {transform: "translateX(10vh)"}; // 22
-        if (value >= 23 && value < 24   ) return {transform: "translateX(8vh)"}; // 23
-        if (value >= 24 && value < 25   ) return {transform: "translateX(6vh)"}; // 24
-        
-        if (value >= 25 && value < 26  ) return {transform: "translateX(4vh)"}; // 25
-        if (value >= 26 && value < 27  ) return {transform: "translateX(2vh)"}; // 26
-        if (value >= 27 && value < 28  ) return {transform: "translateX(1vh)"}; // 27
-        if (value >= 28 && value < 29  ) return {transform: "translateX(0.5vh)"}; // 28
-        if (value >= 29 && value < 30  ) return {transform: "translateX(0vh)"}; // 29
+    const renderMoon = () => {
 
-        return {transform: "translateX(0vh)"};
+        if (value > 0 && value <= 8) {
+            let angle: number = 12.5*value;
+            if (angle > 85 && angle < 110) angle = 90;
+            return (
+                <div className="moon">
+                    <div className="left_hide" ></div>
+                    <div className="disc" style={{transform: `rotateY(${angle}deg)`}}></div>
+                </div>
+            )
+        }
+
+        if (value > 8 && value <= 15)
+            return (
+                <div className="moon">
+                    <div className="disc2" style={{transform: `rotateY(${80 - 12.5 * (value-8)}deg)`}}></div>
+                </div>
+            )
+
+        if (value > 15 && value <= 22)
+            return (
+                <div className="moon">
+                    <div className="disc3" style={{transform: `rotateY(${12.5 * (value-15)}deg)`}}></div>
+                </div>
+            )
+             
+        if (value > 22 && value <= 30) {
+            let angle: number = 90 - 12.5*(value-23);
+            if (angle > 85 && angle < 110) angle = 90;
+            return (
+                <div className="moon">
+                    <div className="right_hide"></div>
+                    <div className="disc4" style={{transform: `rotateY(${angle}deg)`}}></div>
+                </div>
+            )
+        }
     }
-
 
     return (
         <div className="moon_phases_wrapper">
             <SelectedDate />
-            <div className="vertical_rooler"></div>
-            <img className="moon_picture" src={moonImg} alt="moon"  /> 
-            <div className="moon_shadow_wrapper">
-                {/* <div className="moon_shadow"></div> */}
-                <div className="square" style={shadowAcordingTosimulationNumberOfDays()} ></div>
-            </div>
+            {renderMoon()}
         </div>
     )
 }
