f99d057b669f4b6eacfe3ff8fd55f85f1c2b2040
unknown <zoran.zilic.zack@gmail.com>
2022-07-15 23:34:19 +0200

    Remove test elements.

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -6,14 +6,11 @@ import ScreenPositioning from "./ScreenPositioning";
 
 const Board = () => {
     const { isGeorgianCallendar, setIsGeorgianCallendar } = useContext(BoardContext);
-    const { isTestMode, setIsTestMode } = useContext(TestContext);
     const isHorizontal = useUiUxPosition();
     const solarSystemPositionStyle: any = isHorizontal ? {position: "relative"} : {position: "absolute", top: 0};
-    const textModeText: string = isTestMode ? "hide test mode" : "show test mode";
 
     return (
-        <div className="board_wrapper">       
-            <button style={{position: "absolute", top: 0, left: 0, zIndex: 2}} onClick={() => setIsTestMode(!isTestMode)}> {textModeText} </button>      
+        <div className="board_wrapper">           
             <div style={solarSystemPositionStyle}>
                 <CalendarVisualisation />
                 <SolarSystem />
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -4,12 +4,9 @@ import { TestContext } from "../../context";
 import useUiUxPosition from "../customHooks/useUiUxPosition";
 import AdjustEarthToSunAngle from "./AdjustEarthToSunAngle";
 import DatePickerComponent from "./datePicker/DatePickerComponent";
-import HebrewDateInput from "./hebrewDateInput/HebrewDateInput";
 import HebrewDateInput2 from "./hebrewDateInput/HebrewDateInput2";
-import TestComponent from "./TestComponent";
 
 const Sidebar = () => {
-    const { isTestMode } = useContext(TestContext);
     const isHorizontal = useUiUxPosition();
     const sidebarWrapperStyle = isHorizontal ? "sidebar_wrapper font_size" : "sidebar_wrapper_narrow font_size";
     const sidebarStyle = isHorizontal ? "sidebar_wide_screen" : "sidebar_narrow_screen";
@@ -23,10 +20,6 @@ const Sidebar = () => {
                 <DatePickerComponent />
                 
                 <HebrewDateInput2 />
-
-                {/* <HebrewDateInput /> */}
-                
-                {isTestMode && <TestComponent />}
                 
             </div>
         </div>
