903ea9e0aef69b33bb2984818a55b95ef5b1ddd3
unknown <zoran.zilic.zack@gmail.com>
2022-07-15 23:31:25 +0200

    Replace the regular input fields with the antd.

--- src/components/sidebar/datePicker/DatePickerComponent.tsx
+++ src/components/sidebar/datePicker/DatePickerComponent.tsx
@@ -1,29 +1,25 @@
 import { useContext } from "react";
-import DatePicker from "react-datepicker";
+import type { DatePickerProps } from 'antd';
+import { DatePicker, Space } from 'antd';
+// import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { BoardContext } from "../../../context";
 
 const DatePickerComponent = () => {
     const { selectedDateFromDatePicker, setSelectedDateFromDatePicker } = useContext(BoardContext); 
  
-    const processselectedDateFromDatePicker = (date: Date) => {
-        console.log(date);
-        setSelectedDateFromDatePicker(date);
+    const processselectedDateFromDatePicker: DatePickerProps['onChange'] = (date, dateString) => {
+        console.log(date, dateString);
+        if(!dateString) return;
+
+        const selectedDateTypeDate: Date = new Date(dateString);
+        setSelectedDateFromDatePicker(selectedDateTypeDate);
     }
 
     return (
-        <div className="date_picker_wrapper">
+        <div className="date_picker_wrapper" style={{position: "relative"}}>
             <div> PICK GREGORIAN DATE: </div>
-            <DatePicker 
-                selected={selectedDateFromDatePicker} 
-                onChange={(date: Date) => processselectedDateFromDatePicker(date)} 
-                dateFormat="dd/MM/yyyy"
-                isClearable
-                showYearDropdown
-                yearDropdownItemNumber={500}
-                scrollableYearDropdown
-                scrollableMonthYearDropdown
-                />
+            <DatePicker name="gregorian_date" onChange={processselectedDateFromDatePicker} />
         </div>
     );
 
--- src/components/sidebar/hebrewDateInput/HebrewDateInput2.tsx
+++ src/components/sidebar/hebrewDateInput/HebrewDateInput2.tsx
@@ -1,14 +1,21 @@
 import axios from "axios";
-import { useContext, useState } from "react";
+import { useContext } from "react";
+import { 
+    Button,
+    Form,
+    Input,
+    Select,
+   } from "antd";
 import { BoardContext } from "../../../context";
 
 const HebrewDateInput2 = () => {
-    const { selectedDateFromDatePicker, setSelectedDateFromDatePicker } = useContext(BoardContext); 
+    const { setSelectedDateFromDatePicker } = useContext(BoardContext); 
+    const [form] = Form.useForm();
     
-    const [dayNumber, setDayNumber] = useState<number>(1);
-    const [monthName, setMonthName] = useState('Tishrei');
-    const [yearNumber, setYearNumber] = useState<number>(5782);
     const convertDate = () => {
+        const yearNumber: string = form.getFieldValue("hebrewYear");
+        const monthName: string = form.getFieldValue("hebrewMonth");
+        const dayNumber: string = form.getFieldValue("hebrewDay");
         const hebrewToGregorianUrlLink: string = "https://www.hebcal.com/converter?cfg=json&hy="+yearNumber+"&hm="+monthName+"&hd="+dayNumber+"&h2g=1&strict=1";
 
         console.log(hebrewToGregorianUrlLink);
@@ -25,68 +32,69 @@ const HebrewDateInput2 = () => {
 
     const enterDayNumber = () => {
         return (
-            <div style={{display: "flex", justifyContent: "right"}}>
-                <input 
+            <Form.Item name="hebrewDay" label="Input" >
+                <Input
                     type="number" 
-                    min="1"
-                    max="31"
-                    value={dayNumber}
-                    onChange={(event: any) => setDayNumber(event.target.value)}
+                    min={1}
+                    max={31}
                 />
-            </div>
+            </Form.Item>
         )
     }
 
     const enterMonthWord = () => {
         return (
-            <div style={{display: "flex", justifyContent: "right", overflow: "visible"}}>
-                <select value={monthName} onChange={(event: any) => setMonthName(event.target.value)}>
-                    <option value="Nisan">Nisan</option>
-                    <option value="Iyyar">Iyyar</option>
-                    <option value="Sivan">Sivan</option>
-                    <option value="Tamuz">Tamuz</option>
-                    <option value="Av">Av</option>
-                    <option value="Elul">Elul</option>
-                    <option value="Tishrei">Tishrei</option>
-                    <option value="Cheshvan">Cheshvan</option>
-                    <option value="Kislev">Kislev</option>
-                    <option value="Tevet">Tevet</option>
-                    <option value="Shvat">Shvat</option>
-                    <option value="Adar">Adar</option>
-                    <option value="Adar1">Adar1</option>
-                    <option value="Adar2">Adar2</option>
-                </select>
-            </div>
+            <Form.Item name="hebrewMonth" label="Select">
+                <Select>
+                    <Select.Option value="Nisan"> Nisan </Select.Option>
+                    <Select.Option value="Iyyar"> Iyyar </Select.Option>
+                    <Select.Option value="Sivan"> Sivan </Select.Option>
+                    <Select.Option value="Tamuz"> Tamuz </Select.Option>
+                    <Select.Option value="Av">    Av    </Select.Option>
+                    <Select.Option value="Elul">  Elul   </Select.Option>
+                    <Select.Option value="Tishrei"> Tishrei </Select.Option>
+                    <Select.Option value="Cheshvan"> Cheshvan </Select.Option>
+                    <Select.Option value="Kislev">  Kislev  </Select.Option>
+                    <Select.Option value="Tevet"> Tevet  </Select.Option>
+                    <Select.Option value="Shvat"> Shvat </Select.Option>
+                    <Select.Option value="Adar">  Adar  </Select.Option>
+                    <Select.Option value="Adar1"> Adar1 </Select.Option>
+                    <Select.Option value="Adar2"> Adar2 </Select.Option>
+                </Select>
+            </Form.Item>
         )
     }
 
     const enterYearNumber = () => {
         return (
-            <div style={{display: "flex", justifyContent: "right"}}>
-                <input 
-                    type="number" 
-                    min="1"
-                    max="100000"
-                    value={yearNumber}
-                    onChange={(event: any) => setYearNumber(event.target.value)}
+            <Form.Item name="hebrewYear" label="Input">
+                <Input 
+                    type="number"
+                    min={1} 
+                    max={100000}
                 />
-            </div>
+            </Form.Item>
         )
     }
 
     return (
         <div className="hebrew_date_input_wrapper font_size">
 
-            <div className="grid-container">
-                <div className="item1"> PICK HEBREW DATE: </div>
-                <div className="item2"> day </div>
-                <div className="item3"> month </div>  
-                <div className="item4"> year </div>
-                <div className="item5"> {enterDayNumber()} </div>
-                <div className="item6"> {enterMonthWord()} </div>
-                <div className="item7"> {enterYearNumber()} </div>
-                <div className="item8"> <button onClick={convertDate}> convert </button> </div>  
-            </div>            
+            <Form
+                labelCol={{span: 4}}
+                wrapperCol={{span: 14}}
+                layout="horizontal"
+                form={form}
+            >
+                {enterDayNumber()}
+                {enterMonthWord()}
+                {enterYearNumber()}
+
+                <Form.Item label=" ">
+                    <Button onClick={() => convertDate()}> Submit data </Button>
+                </Form.Item>
+            </Form>
+
         </div>
     )
 }
